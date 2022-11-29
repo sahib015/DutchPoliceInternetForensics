26f98be76036df4e1bc6bb8deaac70b50f9bd8df
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-11-29 18:48:41 +0300

    Initial system - redone
    redo initial setup due to server issues

--- DutchPoliceSystem/DutchPoliceSystem/wsgi.py
+++ DutchPoliceSystem/DutchPoliceSystem/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DutchPolice project.
+WSGI config for DutchPoliceSystem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DutchPolice.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DutchPoliceSystem.settings')
 
 application = get_wsgi_application()
