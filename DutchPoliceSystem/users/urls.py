3817d754e398e781ffda50c218a68514ba2c1d36
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-11-29 20:48:58 +0300

    update on login
    basic page for login added. Needs the form and models etc and to load in the /login url instead of home page

--- DutchPoliceSystem/users/urls.py
+++ DutchPoliceSystem/users/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('',views.index, name="index")
+    path('',views.index, name="index"),
+    path('register',views.index,name="register"), #to change to load the registration form
+    path('login',views.index,name="login"), #to change to load the login form
+    path('profile',views.index,name="profile"),# to change to load the profile view
+    path('logout',views.index,name="logout"),# to change the logout
 ]
