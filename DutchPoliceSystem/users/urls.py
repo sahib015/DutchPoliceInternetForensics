9f13c2c59fbba2aaa7a0a000a28a2714cfdabd01
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-11-30 11:19:27 +0300

    Registration form created
    created the registration form

--- DutchPoliceSystem/users/urls.py
+++ DutchPoliceSystem/users/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('',views.index, name="index"),
-    path('register',views.index,name="register"), #to change to load the registration form
-    path('login',views.index,name="login"), #to change to load the login form
-    path('profile',views.index,name="profile"),# to change to load the profile view
-    path('logout',views.index,name="logout"),# to change the logout
+   
+    path('register',views.registerPage,name="register"),
+    path('login',views.loginPage,name="login"),
 ]
--- DutchPoliceSystem/users/views.py
+++ DutchPoliceSystem/users/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
-def index(request):
+def loginPage(request):
     return render(request,'users/login.html')
+
+def registerPage(request):
+    regForm = UserCreationForm()
+    #store data into the databse
+    if request.method=="POST":
+        regForm= UserCreationForm(request.POST)
+        #check if the form is valid before saving to the database
+        if regForm.is_valid():
+            regForm.save() #save details to the database
+    context = {'form':regForm}
+    return render(request,'users/register.html',context)
