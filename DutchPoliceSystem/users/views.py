8f01badb2c497f6134c4b4cc89dba78a9604bd4c
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-11-30 12:29:41 +0300

    registration of user with messages
    registration of user with messages and error messages

--- DutchPoliceSystem/users/views.py
+++ DutchPoliceSystem/users/views.py
@@ -1,7 +1,8 @@
 #required imports
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from .forms import CreateRegistrationForm
 
 # Create your views here.
@@ -17,5 +18,14 @@ def registerPage(request):
         #check if the form is valid before saving to the database
         if regForm.is_valid():
             regForm.save() #save details to the database
+            #get first and last name of user registered
+            firstName = regForm.cleaned_data.get('first_name')
+            lastName = regForm.cleaned_data.get('last_name')
+            name = firstName +' '+lastName
+
+            #pass messages to the dashboard
+            messages.success(request,name+' has successfully been registered and is able to login.')
+
+            return redirect('login') #redirects user to the admin dashboard (to be done later)- temporary redirection to login page
     context = {'form':regForm}
     return render(request,'users/register.html',context)
