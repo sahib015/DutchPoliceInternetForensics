5f7f84efc6e21465c81b30e4689133e18460ec79
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-12-01 09:57:45 +0300

    restructured for the police users
    restructured the url for the police

--- DutchPoliceSystem/DutchPoliceSystem/settings.py
+++ DutchPoliceSystem/DutchPoliceSystem/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #own
-    'users.apps.UsersConfig',
+    'policeUsers.apps.PoliceUsersConfig',
 ]
 
 MIDDLEWARE = [
