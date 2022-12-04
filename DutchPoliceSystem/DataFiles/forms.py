df1ba4f51e86fad4eba7054f790524fbc6f817a1
kwyau126 <kwyau126@yahoo.com.hk>
2022-12-04 20:27:34 +0000

    update ./DataFiles/forms.py and urls.py. add /templates/datafiles/DataUpload.html

--- DutchPoliceSystem/DataFiles/forms.py
+++ DutchPoliceSystem/DataFiles/forms.py
@@ -1,1 +1,8 @@
-#forms to be added. please refer to the policeUsers App
+from django.forms import ModelForm
+from django import forms
+from .models import UploadDataModel
+
+class UploadDataForm(forms.ModelForm):
+    class Meta:
+        model = UploadDataModel
+        fields = ('first_name', 'last_name', 'email', 'title', 'file', 'desc')
