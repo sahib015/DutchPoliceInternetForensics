81fc6cc8e0a56039a1faab792408e223410754df
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-12-12 10:31:59 +0300

    Police Users test Urls
    test urls for police users successful

--- DutchPoliceSystem/policeUsers/tests.py
+++ DutchPoliceSystem/policeUsers/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from policeUsers.views import home, registerPage,loginPage,logoutUser
 
-# Create your tests here.
+#Class for Police users to test URLs 
+class TestUrl(SimpleTestCase):
+    def test_policeDash_url_is_resolved(self):
+        url = reverse('policeDash')
+        self.assertEquals(resolve(url).func,home)
+
+    def test_registerPolice_url_is_resolved(self):
+        url = reverse('registerPolice')
+        self.assertEquals(resolve(url).func,registerPage)
+
+    def test_loginPolice_url_is_resolved(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func,loginPage)
+
+    def test_logoutPolice_url_is_resolved(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func,logoutUser)
