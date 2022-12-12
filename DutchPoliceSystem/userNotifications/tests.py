4206c7ce1ade742066aeb4fb6d2dcb1a5a1a0597
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-12-12 12:05:55 +0300

    user notification test view
    user notification test view

--- DutchPoliceSystem/userNotifications/tests.py
+++ DutchPoliceSystem/userNotifications/tests.py
@@ -1,6 +1,7 @@
-from django.test import SimpleTestCase
+from django.test import SimpleTestCase,TestCase,Client
 from django.urls import reverse, resolve
 from userNotifications.views import createMessage
+from userNotifications.models import CreateNewMessage
 
 # Create your tests here.
 #Class for user notifications to test URLs 
@@ -9,3 +10,22 @@ class TestUrl(SimpleTestCase):
     def test_addmessage_url_is_resolved(self):
         url = reverse('addMessage')
         self.assertEquals(resolve(url).func,createMessage)
+
+#calss for testing the views
+
+class TestView(TestCase):
+    def setup(self):
+        self.client = Client
+      
+
+    def test_createNewMessage_post(self):
+     
+        response = self.client.post(reverse('addMessage'),{
+            'date': '12/12/2022',
+            'name': 'user1',
+            'email': 'user1@test.com',
+            'content': 'this is a dummy test description for testing puroposes',
+            'levelOfPriority': 'low'
+        })
+        self.assertEquals(response.status_code,200)
+        
