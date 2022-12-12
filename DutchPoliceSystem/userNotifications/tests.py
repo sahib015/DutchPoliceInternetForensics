ec89626db03c74025250316536ab8bb3d3db0665
Sahib Jabbal <sahibjabbal@hotmail.com>
2022-12-12 16:47:39 +0300

    removed tests for user notifications forms.py

--- DutchPoliceSystem/userNotifications/tests.py
+++ DutchPoliceSystem/userNotifications/tests.py
@@ -12,22 +12,4 @@ class TestUrl(SimpleTestCase):
         url = reverse('addMessage')
         self.assertEquals(resolve(url).func,createMessage)
 
-#calss for testing the views
 
-
-    
-class TestForm(SimpleTestCase):
-    def test_new_message_form_valid_data(self):
-        form = CreateNewMessageFrom(data={
-            'date':12/12/2022,
-            'name':"user01",
-            'email':"user01@test.com",
-            'content':"this is a dummy test description for testing purposes",
-            'levelOfPriority':"low"
-        })
-        self.assertTrue(form.is_valid())
-
-    def test_new_message_form_invalid_data(self):
-        newMessageForm = CreateNewMessageFrom(data={})
-        self.assertFalse(newMessageForm.is_valid())
-        self.assertEqual(len(newMessageForm.errors),5)
